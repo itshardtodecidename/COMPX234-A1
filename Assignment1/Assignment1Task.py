7063a1dff800aadc48bddff863fadd3e41d236a0
Wang_Honebin <3332721926@qq.com>
2026-03-25 09:07:29 +0800

    feat:task2 v-1

--- Assignment1/Assignment1Task.py
+++ Assignment1/Assignment1Task.py
@@ -20,13 +20,16 @@ class Assignment1:
         self.mThreads = []             # list for machine threads
         self.pThreads = []             # list for printer threads
 
+        self.semaphore=threading.Semaphore(self.NUM_PRINTERS)
+        self.binary=threading.Semaphore(1)
+
     def startSimulation(self):
         # Create Machine and Printer threads
         # Write code here
         for i in range(self.NUM_MACHINES):
             m=self.machineThread(i,self)
             self.mThreads.append[m]
-        
+            
             
         # Start all the threads
         # Write code here
